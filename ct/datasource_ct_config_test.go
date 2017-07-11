cd38ea1671b203a543058cae6771e71250d1a41b
Dalton Hubble <dghubble@gmail.com>
2017-07-11 16:58:32 -0700

    ct: Add platform field to ct_config resource type
    * Set the platform field to enable platform-specific templating, see
    https://github.com/coreos/container-linux-config-transpiler/blob/master/config/templating/templating.go#L27
    * Default platform is "", whhich performs no templating. Appropriate for
    bare-metal and others

--- ct/datasource_ct_config_test.go
+++ ct/datasource_ct_config_test.go
@@ -68,6 +68,46 @@ const prettyExpected = `{
   "passwd": {}
 }`
 
+const ec2Resource = `
+data "ct_config" "ec2" {
+  pretty_print = true
+	platform = "ec2"
+  content  = <<EOT
+---
+etcd:
+  advertise_client_urls:       http://{PUBLIC_IPV4}:2379
+  initial_advertise_peer_urls: http://{PRIVATE_IPV4}:2380
+  listen_client_urls:          http://0.0.0.0:2379
+  listen_peer_urls:            http://{PRIVATE_IPV4}:2380
+  discovery:                   https://discovery.etcd.io/<token>
+EOT
+}
+`
+
+const ec2Expected = `{
+  "ignition": {
+    "version": "2.0.0",
+    "config": {}
+  },
+  "storage": {},
+  "systemd": {
+    "units": [
+      {
+        "name": "etcd-member.service",
+        "enable": true,
+        "dropins": [
+          {
+            "name": "20-clct-etcd-member.conf",
+            "contents": "[Unit]\nRequires=coreos-metadata.service\nAfter=coreos-metadata.service\n\n[Service]\nEnvironmentFile=/run/metadata/coreos\nExecStart=\nExecStart=/usr/lib/coreos/etcd-wrapper $ETCD_OPTS \\\n  --listen-peer-urls=\"http://${COREOS_EC2_IPV4_LOCAL}:2380\" \\\n  --listen-client-urls=\"http://0.0.0.0:2379\" \\\n  --initial-advertise-peer-urls=\"http://${COREOS_EC2_IPV4_LOCAL}:2380\" \\\n  --advertise-client-urls=\"http://${COREOS_EC2_IPV4_PUBLIC}:2379\" \\\n  --discovery=\"https://discovery.etcd.io/\u003ctoken\u003e\""
+          }
+        ]
+      }
+    ]
+  },
+  "networkd": {},
+  "passwd": {}
+}`
+
 func TestRender(t *testing.T) {
 	r.UnitTest(t, r.TestCase{
 		Providers: testProviders,
@@ -76,6 +116,12 @@ func TestRender(t *testing.T) {
 				Config: prettyResource,
 				Check: r.ComposeTestCheckFunc(
 					r.TestCheckResourceAttr("data.ct_config.example", "rendered", prettyExpected),
+				),
+			},
+			r.TestStep{
+				Config: ec2Resource,
+				Check: r.ComposeTestCheckFunc(
+					r.TestCheckResourceAttr("data.ct_config.ec2", "rendered", ec2Expected),
 				),
 			},
 		},
